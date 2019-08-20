62b67a262459bc9908518061c6ca3f10c6bb7f91
Matheus Aprigio <matheusaprig@gmail.com>
2019-08-19 22:57:51 -0300

    Add application.properties

---
+++ src/main/java/br/com/buildit/BuilditApplication.java
@@ -0,0 +1,13 @@
+package br.com.buildit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuilditApplication.class, args);
+    }
+
+}
