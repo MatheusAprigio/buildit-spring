e87ff9e5ecb41875c50ba426a3206f267bf395dc
MatheusAprigio <matheusaprig@gmail.com>
2019-09-30 12:25:53 -0300

    Add js modal update

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,13 +9,20 @@ $(document).ready(function(){
     $('.modal').modal();
 });
 
-
 $(".btn-remove").click(function () {
     var id = $(this).siblings().val();
     $("#delete-id").val(id);
-})
+});
 
 $(".btn-edit").click(function () {
     var id = $(this).siblings().val();
     $("#edit-id").val(id);
-})
+});
+
+$(".edit-admin").click(function () {
+    var AdminName = $(this).closest("tr").find('td:eq(0)').text();
+    $("#name").val(AdminName);
+
+    var email = $(this).closest("tr").find('td:eq(1)').text();
+    $("#email").val(email)
+});
