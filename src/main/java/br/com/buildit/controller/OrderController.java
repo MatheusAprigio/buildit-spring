5fb69d76ccdb236459244aad776fad9f87452a1a
MatheusAprigio <matheusaprig@gmail.com>
2019-10-09 21:22:35 -0300

    Add cancel order

--- src/main/java/br/com/buildit/controller/OrderController.java
+++ src/main/java/br/com/buildit/controller/OrderController.java
@@ -1,15 +1,18 @@
 package br.com.buildit.controller;
 
+import br.com.buildit.model.Order;
 import br.com.buildit.model.Status;
 import br.com.buildit.repository.OrderProductRepository;
 import br.com.buildit.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.persistence.EntityNotFoundException;
 
 @Controller
 @RequestMapping("order")
@@ -47,5 +50,17 @@ public class OrderController {
 
         model.addAttribute("orderProduct", orderProductRepository.findAll());
         return "forms/orders";
+    }
+
+    @PostMapping("cancel")
+    public String cancelOrder(Integer id, RedirectAttributes redirectAttributes) {
+
+        Order order = orderRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
+        order.setStatus(Status.CANCELADO);
+        orderRepository.save(order);
+
+        redirectAttributes.addFlashAttribute("msg", "Pedido cancelado com sucesso!");
+
+        return "redirect:list";
     }
 }
--- src/main/java/br/com/buildit/repository/OrderRepository.java
+++ src/main/java/br/com/buildit/repository/OrderRepository.java
@@ -3,6 +3,8 @@ package br.com.buildit.repository;
 import br.com.buildit.model.Order;
 import br.com.buildit.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +19,9 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
     List<Order> findByCustomer_NameIsContainingIgnoreCaseOrCustomer_EmailIsContainingIgnoreCaseOrDriver_NameIsContainingIgnoreCaseOrDriver_EmailIsContainingIgnoreCase(String customerName, String customerEmail, String driverName, String driverEmail);
 
     List<Order> findByStatusAndCustomer_NameIsContainingIgnoreCaseOrCustomer_EmailIsContainingIgnoreCaseOrDriver_NameIsContainingIgnoreCaseOrDriver_EmailIsContainingIgnoreCase(Status status, String customerName, String customerEmail, String driverName, String driverEmail);
+
+    @Query("Update Order o set o.status  = 'CANCELADO' where o.id = ?1")
+    @Modifying
+    Order updateOrderStatus(Integer id);
+
 }
